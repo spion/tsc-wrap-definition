5d0d47d8b3af1b06c27484f9cec0366d0a40225e
Gorgi Kosev <gorgi.kosev@gmail.com>
2015-01-16 12:22:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ input.pipe(bl(function(err, data) {
     var processed = data.toString().split(/[\r\n]+/)
     .map(function(line) { return line.replace(/^export declare/, 'export'); })
     .join('\n');
-    output.write('declare module "' + process.argv[process.argv.length - 1] + '" {\n')
+    output.write('declare module "' + modname + '" {\n')
     output.write(processed);
     output.write('}\n');
 }));
